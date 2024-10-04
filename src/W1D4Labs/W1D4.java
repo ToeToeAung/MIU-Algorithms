e3aafffa3444311835208ec8bfab0699f8e2b382
ToeToeAung <toetoeag@gmail.com>
2024-10-03 21:49:29 -0500

    Algoritms - DP - ClimbStairs

---
+++ src/W1D4Labs/W1D4.java
@@ -0,0 +1,22 @@
+package W1D4Labs;
+
+public class W1D4 {
+
+	public static void main(String[] args) {
+		int step=ClimbStairs(10);
+		System.out.println("Number of steps " +step);
+	}
+	  public static int ClimbStairs(int n) {
+	      if(n <= 2 )
+	          return n;
+	       
+	        int a=1;
+	        int b=2;
+	        for(int i=3;i<=n;i++){
+	            int sum = a + b;
+	            a = b;
+	            b = sum;
+	        }
+	        return b;
+	    }
+}
