98cacfa87ada72cf7e29c038bce52db45ae07fc4
ToeToeAung <toetoeag@gmail.com>
2024-10-19 22:00:13 -0500

    Leetcode Network Delay - Test

--- src/LeetCode/BooleanExpr.java
+++ src/LeetCode/BooleanExpr.java
@@ -3,8 +3,6 @@ package LeetCode;
 public class BooleanExpr {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		boolean result = parseBoolExpr("&(|(f))");
 		System.out.println("Result " + result);
 	}
--- src/Tutorials/NetworkDelay.java
+++ src/Tutorials/NetworkDelay.java
@@ -3,10 +3,12 @@ import java.util.*;
 public class NetworkDelay {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	  public int networkDelayTime(int[][] times, int n, int k) {
+		int n= 4,k=2;
+		int[][] times ={{2,1,1},{2,3,1},{3,4,1}};
+		int totalDelay=networkDelayTime (times,n,k);
+		System.out.println("Network Delay Time " + totalDelay);
+		}
+	  public static int networkDelayTime(int[][] times, int n, int k) {
 	        Map<Integer, List<int[]>> graph = new HashMap<>();
 	        for (int[] time : times) {
 	            int u = time[0], v = time[1], t = time[2];
@@ -14,11 +16,16 @@ public class NetworkDelay {
 	        }
 
 	        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+	      
 	        minHeap.add(new int[]{k, 0});
 	        Map<Integer, Integer> minTimes = new HashMap<>();
 
 	        while (!minHeap.isEmpty()) {
 	            int[] current = minHeap.poll();
+	            for(int a :current) {
+	            	 System.out.println(" Heap Values *** "+ a);
+	            }
+	           
 	            int node = current[0];
 	            int time = current[1];
 
@@ -27,10 +34,13 @@ public class NetworkDelay {
 	            minTimes.put(node, time);
 	            if (graph.containsKey(node)) {
 	                for (int[] neighbor : graph.get(node)) {
-	                    int nei = neighbor[0];
-	                    int neiTime = neighbor[1];
-	                    if (!minTimes.containsKey(nei)) {
-	                        minHeap.add(new int[]{nei, time + neiTime});
+	                    int nb = neighbor[0];
+	                    int nbTime = neighbor[1];
+	                    
+	                    System.out.println(" Heap Values *** nb "+ nb + " - nbTime "+ nbTime);
+	                    
+	                    if (!minTimes.containsKey(nb)) {
+	                        minHeap.add(new int[]{nb, time + nbTime});
 	                    }
 	                }
 	            }
